acf822f9e749cfc17c119b319a416b10e415c5b5
Daniel Della-Rose <danieldella-rose@Daniels-MacBook-Pro.local>
2020-09-01 17:28:14 -0600

    updated css and git

---
+++ app.js
@@ -0,0 +1,54 @@
+
+
+$(() => {
+  $('.button').on('click', event => {
+    preventDefault()
+  })
+
+  // all element of modal set to variables
+  const $jokeBtn = $('#jokeBtn')
+  let $jokeModal = $('#jokeModal')
+  let jokeText = ""
+
+
+  const $openBtn = $('#bioModal')
+  const $chuckModal = $('#chuckModal')
+  const $closeBtn = $('#close')
+  const $closeJoke = $('#closeJoke')
+
+    // my event handlers
+    const openModal = () => {
+      $chuckModal.css('display', 'block')
+    }
+
+
+    const closeModal = () => {
+      $chuckModal.css('display', 'none')
+
+
+    }
+    const closeJokeModal = () => {
+      $jokeModal.css('display', 'none')
+    }
+// I couldn't get the ajax method to work regarding syntax errors. I found this method to work.
+    const getRandomJoke = () => {
+      $.get("http://api.icndb.com/jokes/random", (data) => {
+        $('#joke-text').text(data.value.joke)
+      })
+
+      $jokeModal.css('display', 'block')
+    //
+    }
+
+    // my event listeners
+    $openBtn.on('click', openModal)
+    $closeBtn.on('click', closeModal)
+    $jokeBtn.on('click', getRandomJoke)
+    $closeJoke.on('click', closeJokeModal)
+
+    setTimeout(openModal, 5000);
+
+
+
+
+})
